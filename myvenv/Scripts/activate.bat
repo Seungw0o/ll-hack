018a83a4598871d5ad8229cf4131b28903461b3b
Your Name <you@example.com>
2020-10-15 21:13:33 +0900

    CRUD first commit

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tmddn\Desktop\멋쟁이사자처럼\coding\lotte-hack\myvenv
+set VIRTUAL_ENV=C:\Users\tmddn\Desktop\멋쟁이사자처럼\coding\likelion-lottehack\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
